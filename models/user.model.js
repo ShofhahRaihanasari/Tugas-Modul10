e54e0aaeebc0df8899bce532505696c275689a03
[Shofhah Raihanasari] <a123@123de-MacBook-Pro.local>
2023-10-24 01:27:38 +0800

    commit tugas individu modul 10

---
+++ models/user.model.js
@@ -0,0 +1,47 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define('devices', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+          },
+        
+          name: {
+            type: Sequelize.STRING,
+            allowNull: true,
+          },
+
+          location: {
+            type: Sequelize.STRING,
+            allowNull: true,
+          },
+
+          type: {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+          },
+
+          sensor_spesification: {
+            type: Sequelize.STRING,
+            allowNull: true,
+          },
+
+          gps_location_longitude: {
+            type: Sequelize.DECIMAL(10),
+            allowNull: true,
+          },
+
+          gps_location_latitude: {
+            type: Sequelize.DECIMAL(10),
+            allowNull: true,
+          },
+          
+          device_notification: {
+            type: Sequelize.TINYINT,
+            allowNull: true,
+          }
+    });
+
+    return User;
+}
